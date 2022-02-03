a59b08b1812272561608c02c9f6765ef34a76f70
Umair Ali <muhammad_umair_ali_malhi@live.com>
2022-02-03 19:04:08 +0500

    Added Middleware

--- src/api/1.0/routers/UserRouter.js
+++ src/api/1.0/routers/UserRouter.js
@@ -2,7 +2,33 @@ const express = require('express');
 const router = express.Router();
 const RegisterUser = require('../../../domains/users/actions/Register');
 
-router.post('/api/1.0/users', async (req, res) => {
+const validateUserRequest = (req, res, next) => {
+  const body = req.body;
+  if (body.username === undefined || body.username === null) {
+    return res.status(400).send({
+      message: 'User could not be Created.',
+      error: 'User could not be Created because the User data is Invalid.',
+      validationErrors: {
+        username: 'Username cannot be null',
+      },
+      success: false,
+    });
+  }
+  if (body.email === undefined || body.email === null) {
+    return res.status(400).send({
+      status: 400,
+      message: 'User could not be Created.',
+      error: 'User could not be Created because the User data is Invalid.',
+      validationErrors: {
+        email: 'Email cannot be null',
+      },
+      success: false,
+    });
+  }
+  next();
+};
+
+router.post('/api/1.0/users', validateUserRequest, async (req, res) => {
   const response = await RegisterUser.__invoke(req.body);
   return res.status(response.status).send({
     message: response.message,
--- src/domains/users/actions/Register.js
+++ src/domains/users/actions/Register.js
@@ -2,28 +2,6 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
 const __invoke = async (body) => {
-  if (body.username === undefined || body.username === null) {
-    return {
-      status: 400,
-      message: 'User could not be Created.',
-      error: 'User could not be Created because the User data is Invalid.',
-      validationErrors: {
-        username: 'Username cannot be null',
-      },
-      success: false,
-    };
-  }
-  if (body.email === undefined || body.email === null) {
-    return {
-      status: 400,
-      message: 'User could not be Created.',
-      error: 'User could not be Created because the User data is Invalid.',
-      validationErrors: {
-        email: 'Email cannot be null',
-      },
-      success: false,
-    };
-  }
   try {
     const hash = await bcrypt.hash(body.password, 10);
     const user = { ...body, password: hash };
