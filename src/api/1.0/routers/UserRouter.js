cb85a2d9669e4c09ee7fbe010adecdd02f0aec00
Umair Ali <muhammad_umair_ali_malhi@live.com>
2022-03-02 13:52:33 +0500

    Separated Validators

--- src/api/1.0/routers/UserRouter.js
+++ src/api/1.0/routers/UserRouter.js
@@ -2,57 +2,29 @@ const express = require('express');
 const router = express.Router();
 const UserService = require('../../../domains/users/UserService');
 
-const { check, validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 const InvalidTokenException = require('../../../domains/users/exceptions/InvalidTokenException');
 const ValidationException = require('../../../domains/common/exceptions/ValidationException');
+const UsernameValidator = require('../../../domains/users/validators/UsernameValidator');
+const EmailValidator = require('../../../domains/users/validators/EmailValidator');
+const PasswordValidator = require('../../../domains/users/validators/PasswordValidator');
 
-router.post(
-  '/api/1.0/users',
-  check('username')
-    .notEmpty()
-    .withMessage('username_null')
-    .bail()
-    .isLength({ min: 6, max: 32 })
-    .withMessage('username_size'),
-  check('email')
-    .notEmpty()
-    .withMessage('email_null')
-    .bail()
-    .isEmail()
-    .withMessage('email_invalid')
-    .bail()
-    .custom(async (email) => {
-      const user = await UserService.findByEmail(email);
-      if (user) {
-        throw new Error('email_inuse');
-      }
-    }),
-  check('password')
-    .notEmpty()
-    .withMessage('password_null')
-    .bail()
-    .isLength({ min: 8, max: 128 })
-    .withMessage('password_size')
-    .bail()
-    .isStrongPassword()
-    .withMessage('password_pattern'),
-  async (req, res, next) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return next(new ValidationException(req.t('user_datainvalid'), errors.array()));
-      }
-      const response = await UserService.register(req.body);
-      return res.status(response.status).send({
-        message: req.t(response.message),
-        user: response.user,
-        success: response.success,
-      });
-    } catch (error) {
-      next(error);
+router.post('/api/1.0/users', UsernameValidator, EmailValidator, PasswordValidator, async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(new ValidationException(req.t('user_datainvalid'), errors.array()));
     }
+    const response = await UserService.register(req.body);
+    return res.status(response.status).send({
+      message: req.t(response.message),
+      user: response.user,
+      success: response.success,
+    });
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 router.post('/api/1.0/users/token/:token', async (req, res, next) => {
   try {
